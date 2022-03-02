a848a1d9824d828e2a2ad7f95725072b04cba3bf
CMJ Grubb <55161169+cmjgrubb@users.noreply.github.com>
2022-03-02 09:13:04 -0500

    Update microsoftUpdate.ps1

--- microsoftUpdate.ps1
+++ microsoftUpdate.ps1
@@ -2,5 +2,7 @@
 Author: CMJ Grubb
 Date: 03/01/2022
 Description: This script runs Microsoft Update, including Sharepoint updates.
+Dependencies: Requires PSWindowsUpdate by Michal Gajda - https://www.powershellgallery.com/packages/PSWindowsUpdate/2.0.0.4
 #>
+Import-Module PSWindowsUpdate
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll -AutoReboot -Confirm:$false -ForceInstall

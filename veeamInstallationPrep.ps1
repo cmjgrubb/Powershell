530917c92a1af88263b5c865a36461de1686d0bb
CMJ Grubb <55161169+cmjgrubb@users.noreply.github.com>
2022-09-01 10:04:17 -0400

    Create veeamInstallationPrep.ps1

---
+++ veeamInstallationPrep.ps1
@@ -0,0 +1,32 @@
+ <#
+Author: CMJ Grubb
+Date: 09/01/2022
+
+Description: This script prepares a non-domain computer for Veeam Backup and Replication.
+#>
+
+# Enable file and printer sharing
+Set-NetFirewallRule -DisplayGroup "File And Printer Sharing" -Enabled False -Profile Any
+
+# Disable SMB1 and enable SMB 2/3
+Set-SmbServerConfiguration -EnableSMB1Protocol $false
+Set-SmbServerConfiguration -EnableSMB2Protocol $true
+
+# Variables for the registry settings that need to be added
+$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
+$Name = "LocalAccountTokenFilterPolicy"
+$value = "1"
+
+# Create the registry item if it doesn't exist
+# Set the value to 1
+IF(!(Test-Path $registryPath))
+{
+    New-Item -Path $registryPath -Force | Out-Null
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null
+}
+ELSE
+{
+    New-ItemProperty -Path $registryPath -Name $name -Value $value `
+    -PropertyType DWORD -Force | Out-Null
+} 

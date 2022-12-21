65c5280867615b79bd71e8f537237fa1292d7df1
CMJ Grubb <55161169+cmjgrubb@users.noreply.github.com>
2022-12-21 14:00:09 -0500

    Update accountLockoutNotification.ps1
    E-mail was only being sent to the last recipient in the array, so I wrapped Send-MailMessage in a ForEach loop. I also removed the computer name, as I am running this locally on each of the domain controllers.

--- accountLockoutNotification.ps1
+++ accountLockoutNotification.ps1
@@ -5,9 +5,9 @@ Description: This script send an E-mail when an AD account has been locked out.
 #>
 
 $From = "smtp@contoso.com"                
-$To = "craig.grubb@contoso.com", "brian.thompson@contoso.com", "wesley.gardner@contoso.com"
+$Recipients = "craig.grubb@contoso.com", "brian.thompson@contoso.com", "wesley.gardner@contoso.com"
 
-$Info = Get-WinEvent -ComputerName 'dc1' -MaxEvents 1 -FilterHashTable @{ LogName = "Security"; ID = 4740 } |  %{([xml]$_.ToXml()).Event.EventData.Data } | Out-String
+$Info = Get-WinEvent -MaxEvents 1 -FilterHashTable @{ LogName = "Security"; ID = 4740 } |  %{([xml]$_.ToXml()).Event.EventData.Data } | Out-String
             
 $Subject = "Account Locked Out" 
 $Body = "A user account has been locked out:`n" + $Info
@@ -18,4 +18,6 @@ $username="smtp@contoso.com"
 $password = Get-Content -Path $EncryptedPasswordFile | ConvertTo-SecureString
 $credential = New-Object System.Management.Automation.PSCredential($username, $password)
 
-Send-MailMessage -ErrorAction Stop -from "$From" -to "$To" -subject "$Subject" -body "$Body" -SmtpServer "$SMTPserver" -Priority  "Normal" -Credential $credential -Port 587 -UseSsl 
+ForEach($Recipient in $Recipients) {
+ Send-MailMessage -ErrorAction Stop -from "$From" -to "$Recipient" -subject "$Subject" -body "$Body" -SmtpServer "$SMTPserver" -Priority  "Normal" -Credential $credential -Port 587 -UseSsl
+} 
